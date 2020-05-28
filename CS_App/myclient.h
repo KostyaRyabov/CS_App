0e4a356f2ea57af7ae81a21fd0ec7c1435cd005c
Kostya <45066193+KostyaRyabov@users.noreply.github.com>
2020-05-28 21:55:31 +0300

    init

---
+++ CS_App/myclient.h
@@ -0,0 +1,16 @@
+#ifndef MYCLIENT_H
+#define MYCLIENT_H
+
+#include <QObject>
+
+class MyClient : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyClient(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MYCLIENT_H
